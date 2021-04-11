b3a870c8bdea0220061644b9ca6bf9a74302fa1d
laurens <>
2021-04-11 17:24:33 +0200

    Moved app styling to seperate file

---
+++ src/components/App/App.styles.ts
@@ -0,0 +1,40 @@
+export const styles = {
+  "@global": {
+    body: {
+      margin: 0,
+    },
+    ul: {
+      margin: 0,
+      padding: 0,
+    },
+    li: {
+      margin: 0,
+      padding: 0,
+      textIndent: 0,
+      listStyle: "none",
+    },
+  },
+
+  app: {
+    textAlign: "center",
+  },
+
+  "app-logo": {
+    height: "40vmin",
+    pointerEvents: "none",
+  },
+  appHeader: {
+    backgroundColor: "#282c34",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white",
+  },
+
+  appLink: {
+    color: "#61dafb",
+  },
+};
