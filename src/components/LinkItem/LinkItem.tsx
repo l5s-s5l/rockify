1ad172876bf0713911584db761a3c61034e042a5
l5s-s5l <laurens.sprakel@gmail.com>
2021-05-18 20:37:21 +0200

    added url as prop

--- src/components/LinkItem/LinkItem.tsx
+++ src/components/LinkItem/LinkItem.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 interface LinkItem {
   name: string | undefined;
+  url: string
 }
 
-
 function LinkItem(props: LinkItem): JSX.Element {
-  const { name } = props;
+  const { name, url } = props;
 
   return (
-    <Link data-testid="link-list-item" to={`/artist/${name}`}>
+    <Link data-testid="link-list-item" to={url}>
       <span>{name}</span>
     </Link>
   );
