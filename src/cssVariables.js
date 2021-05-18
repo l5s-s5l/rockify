ae1102d32adc036d44f920803aa4decd5ee099d1
l5s-s5l <laurens.sprakel@gmail.com>
2021-05-18 20:36:36 +0200

    added css variables

--- src/cssVariables.js
+++ src/cssVariables.js
@@ -4,10 +4,12 @@ export const color = {
   lightGrey: "#f0f0f0",
   white: "#fff",
   rockYellow: "#FFE000",
+  blue: '#1976d2'
 };
 
 export const fontSize = {
   small: 14,
+  smallMedium: 18,
   medium: 24,
   Large: 32,
 };
