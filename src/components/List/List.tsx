852721f6141825e809aba62b88b5f2df0f686083
l5s-s5l <laurens.sprakel@gmail.com>
2021-05-18 21:08:23 +0200

    added new data type to list

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -8,25 +8,26 @@ import { CTA, LINK } from "const";
 const useStyles = createUseStyles(styles, { name: "List" });
 
 interface List {
-  data: Artist[] | Song[] | null;
+  data: Artist[] | Song[] | string[] | null;
   type: typeof CTA | typeof LINK;
+  contentType?: string | undefined
 }
 
 function List(props: List): JSX.Element {
   const classes = useStyles();
-  const { data, type } = props;
+  const { data, type, contentType } = props;
 
   if (Array.isArray(data) && !data.length) {
-    return <span> no items found</span>;
+    return <div className={classes.placeholder}>Create a new list</div>;
   }
 
   return (
     <>
       <ul className={classes.list}>
         {Array.isArray(data) &&
-          data.map((listItem: Artist | Song) => (
-            <li className={classes.listItem} key={listItem.id}>
-              <ListItem type={type} listItem={listItem} />
+          data.map((listItem: Artist | Song | string) => (
+            <li className={classes.listItem} key={typeof listItem === 'string' ? listItem : listItem.id}>
+              <ListItem type={type} listItem={listItem} contentType={contentType} />
             </li>
           ))}
       </ul>
